4af830a61074be20ed71d984bea0a6c6494a13a6
Rhythm Mehta <40776621+rhythmehta@users.noreply.github.com>
2022-04-04 13:28:46 -0700

    Update __init__.py

--- kanban/__init__.py
+++ kanban/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
-from kanban import routes
 
 app = Flask(__name__, instance_relative_config=True)
 app.config['SECRET_KEY'] = 'secretkeynoonewillknow'
@@ -14,3 +13,5 @@ bcrypt = Bcrypt()
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = "info"
+
+from kanban import routes
